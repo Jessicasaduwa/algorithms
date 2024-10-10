22ccd7c1c6818d9b8f7c0d2d097079eb48d730fb
Jessicasaduwa <jessicasaduwa@gmail.com>
2024-10-10 13:44:04 +0100

    algorithm updates

--- variable.js
+++ variable.js
@@ -35,3 +35,22 @@ console.log("Number of vowels:", vowels);
 
 let sentence = "This is a sentence.";
 processSentence(sentence);
+
+function insertionSort(arr) {
+for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+        arr[j + 1] = key;
+}
+    return arr;
+}
+
+let arr = [12, 11, 13, 5, 6];
+console.log("Original Array:", arr);
+
+let sortedArray = insertionSort(arr);
+console.log("Sorted Array:", sortedArray);
